e16ae73f4ee89c22c97c3c13366e71bc09e05648
kaitlynmarcatante <kaitlynmarcatante@gmail.com>
2025-08-29 16:18:20 -0500

    Update 1

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("What's the most valuable thing we should take out of this class?");
+
+document.write("<h1>Hi from Maleah!</h1>");
+document.write(
+  "<p>Hope all is well and that things are off to a good semester.</p>"
+);
